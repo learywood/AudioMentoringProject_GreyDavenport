01df25e6b8b16e46ba2f4fca009d531fd3551aed
greydavenport <gkdavenport@gmail.com>
2018-02-11 00:29:36 -0800

    Added befinings of audio programming
    Working on Delay line, I'm not sure its doing what I think it's doing. I'm having a bit of trouble testing.

---
+++ UFOShooter/Source/UFOShooter/MySynthComponent.cpp
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MySynthComponent.h"
+
+MyDelay* delay = new MyDelay(1.0f, 0.50f);
+
+void UMySynthComponent::Init(const int32 SampleRate)
+{
+	NumChannels = 1;
+
+	// Initialize the DSP objects
+	Osc.Init(SampleRate);
+	Osc.SetFrequency(440.0f);
+	Osc.SetGain(1.0f);
+	Osc.Start();
+
+}
+
+MyDelay::MyDelay(float time, float decay) : position(0), decay(decay)
+{
+	size = (int)(time * 44100);
+	buffer = new float[size];
+	memset(buffer, 0, size);
+}
+
+MyDelay::~MyDelay() {
+	delete[] buffer;
+}
+
+void MyDelay::WriteSoundData(float* data, int count) {
+	for (int i = 0; i < count; ++i) {
+		// Mix sample with the one stored in the buffer at position
+		data[i] = (float)(data[i] + buffer[position] * decay);
+		// Record this new value in the buffer at position
+		buffer[position] = data[i];
+		// Increment buffer position wrapping around
+		++position;
+		if (position >= size)
+			position = 0;
+	}
+}
+
+void UMySynthComponent::OnGenerateAudio(TArray<float>& OutAudio)
+{
+	
+	float size = DelayValue * 44100;
+	int32* buffer = new int32[(int)size];
+	memset(buffer, 0, size);
+	int CurrentIndex = 0;
+	float decay = 1;
+	float decayint = decay;
+
+	// Perform DSP operations here
+	for (int32 Sample = 0; Sample < OutAudio.Num(); ++Sample)
+	{
+		if (DelayToggle == true)
+		{
+			OutAudio[Sample] = (buffer[CurrentIndex] + Osc.Generate()) * 0.5f;
+			buffer[CurrentIndex] = OutAudio[Sample];
+			
+			if (CurrentIndex == size)
+			{
+				CurrentIndex = 0;
+			}
+			
+			//UE_LOG(LogTemp, Warning, TEXT("Delay %d"), buffer[CurrentIndex]);
+			
+			CurrentIndex++;
+		}
+		else
+		{
+			OutAudio[Sample] = Osc.Generate();
+		}
+	}
+}
+
+void UMySynthComponent::SetFrequency(const float InFrequencyHz)
+{
+	// Use this protected base class method to push a lambda function which will safely execute in the audio render thread.
+	SynthCommand([this, InFrequencyHz]()
+	{
+		Osc.SetFrequency(InFrequencyHz);
+		Osc.Update();
+	});
+}
+
+void UMySynthComponent::SetGain(const float Gain)
+{
+	// Use this protected base class method to push a lambda function which will safely execute in the audio render thread.
+	SynthCommand([this, Gain]()
+	{
+		Osc.SetGain(Gain);
+		Osc.Update();
+	});
+}
---
+++ UFOShooter/Source/UFOShooter/MySynthComponent.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SynthComponent.h"
+#include "DSP/Osc.h"
+#include "MySynthComponent.generated.h"
+
+// ========================================================================
+// UMySynthComponent
+// Synth component class which implements USynthComponent
+// This is a simple hello-world type example which generates a sine-wave
+// tone using a DSP oscillator class and implements a single function to set
+// the frequency. 
+// ========================================================================
+
+UCLASS(ClassGroup = Synth, meta = (BlueprintSpawnableComponent))
+class UFOSHOOTER_API UMySynthComponent : public USynthComponent
+{
+	GENERATED_BODY()
+	
+	// Called when synth is created
+	virtual void Init(const int32 SampleRate) override;
+
+	// Called to generate more audio
+	virtual void OnGenerateAudio(TArray<float>& OutAudio) override;
+
+	// Sets the oscillator's frequency
+	UFUNCTION(BlueprintCallable, Category = "Synth|Components|Audio")
+	void SetFrequency(const float FrequencyHz = 440.0f);
+
+	UFUNCTION(BlueprintCallable, Category = "Synth|Components|Audio")
+		void SetGain(const float Gain = 1.0f);
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Delay")
+		bool DelayToggle = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Delay")
+		float DelayValue = 1.0f;
+
+
+protected:
+	// A simple oscillator class. Can also generate Saw/Square/Tri/Noise.
+	Audio::FOsc Osc;
+};
+
+class MyDelay {
+	public: 
+		MyDelay(float time, float decay);
+		~MyDelay();
+		void WriteSoundData(float* data, int count);
+
+	private:
+		float* buffer;
+		int size;
+		int position;
+		float decay;
+};
